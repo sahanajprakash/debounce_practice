11376bd005aa3f0bfaa571829945f2b1641a8289
sahanajayadevaprakash <65385037+sahanajprakash@users.noreply.github.com>
2024-10-14 11:58:00 -0400

    adding delay

--- script.js
+++ script.js
@@ -3,6 +3,19 @@ const defaultText = document.getElementById("default")
 const debounceText = document.getElementById("debounce")
 const throttleText = document.getElementById("throttle")
 
+const updateDebounceText = debounce((text) => {
+    debounceText.textContent = text
+})
+
 input.addEventListener('input', e => {
     defaultText.textContent = e.target.value
+    updateDebounceText(e.target.value)
 })
+
+function debounce(cb, delay = 1000) {
+    return (...args) => {
+        setTimeout(() => {
+            cb(...args)
+        }, delay)
+    }
+}
