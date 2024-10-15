1c435d9d6155c7bc9b763934b7b0287571ff58f6
sahanajayadevaprakash <65385037+sahanajprakash@users.noreply.github.com>
2024-10-15 09:59:35 -0400

    throttle practice

--- script.js
+++ script.js
@@ -6,16 +6,51 @@ const throttleText = document.getElementById("throttle")
 const updateDebounceText = debounce((text) => {
     debounceText.textContent = text
 })
+const updateThrottleText = throttle((text) => {
+    throttleText.textContent = text
+})
+
 
 input.addEventListener('input', e => {
     defaultText.textContent = e.target.value
     updateDebounceText(e.target.value)
+    updateThrottleText(e.target.value)
 })
 
 function debounce(cb, delay = 1000) {
+    let timeout
+
     return (...args) => {
-        setTimeout(() => {
+        clearTimeout(timeout)
+        timeout = setTimeout(() => {
             cb(...args)
         }, delay)
     }
+}
+
+function throttle(cb, delay = 100) {
+    let shouldWait = false
+    let waitingArgs
+    const timeoutFunc = () => {
+        if (waitingArgs == null) {
+            shouldWait = false
+        } else {
+            cb(...waitingArgs)
+            waitingArgs = null
+            setTimeout(timeoutFunc, delay)
+        }
+    }
+    return (...args) => {
+        if (shouldWait) {
+            waitingArgs = args
+            return
+        }
+
+        cb(...args)
+        shouldWait = true
+
+        setTimeout(timeoutFunc, delay)
+    }
+}
+
 }
